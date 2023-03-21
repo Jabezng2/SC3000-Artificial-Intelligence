1b1592a02fc485aa3a96a816d7ae77af8b81fa3c
HLeong12345 <100272517+HLeong12345@users.noreply.github.com>
2023-03-21 16:29:24 +0800

    Add files via upload

--- Assignment 2/royalFamily1.pl
+++ Assignment 2/royalFamily1.pl
@@ -1,50 +1,28 @@
 male(charles).
-male(edward).
 male(andrew).
-
-female(elizabeth).
+male(edward).
 female(ann).
+offspring(elizabeth,charles).
+offspring(elizabeth,ann).
+offspring(elizabeth,andrew).
+offspring(elizabeth,edward).
+queen(elizabeth).
+age(charles,ann,andrew,edward).
 
-child(charles, elizabeth).
-child(ann, elizabeth).
-child(andrew, elizabeth).
-child(edward, elizabeth).
+prince(X) :-
+    offspring(Y,X),
+    queen(Y),
+    male(X).
 
-older_than(charles, ann).
-older_than(charles, andrew).
-older_than(charles, edward).
-older_than(ann, andrew).
-older_than(ann, edward).
-older_than(andrew, edward).
+princess(X) :-
+    offspring(Y,X),
+    queen(Y),
+    female(X).
 
-son(X,Y) :- child(X,Y), male(X).
-
-daughter(X,Y) :- child(X,Y), female(X).
-
-successor(X, Y):- child(Y,X).
-%successor(X, Y):- son(Y,X).
-%successor(X, Y):- daughter(Y,X).
-
-successionList(X, SuccessionList):-
-	findall(Y, successor(X, Y), SuccessionList).
-
-precedes(X,Y):- male(X), male(Y), older_than(X,Y).
-precedes(X,Y):- male(X), female(Y), Y\=elizabeth.
-precedes(X,Y):- female(X), female(Y), older_than(X,Y).
-
-%precedes(X,Y):- older_than(X,Y).
-
-%% Sorting algorithm
-succession_sort([A|B], Sorted) :- succession_sort(B, SortedTail), insert(A, SortedTail, Sorted).
-succession_sort([], []).
-
-insert(A, [B|C], [B|D]) :- not(precedes(A,B)), !, insert(A, C, D).
-insert(A, C, [A|C]).
-
-
-
-
-successionListIndependent(X, SuccessionList):-
-	findall(Y, child(Y,X), Children),
-	succession_sort(Children, SuccessionList).
-
+successionLine(Who) :-
+    (age(X,Y,Z,W),
+    prince(Who),
+    (Who=X;Who=Y;Who=Z;Who=W));
+    (age(X,Y,Z,W),
+     princess(Who),
+    (Who=X;Who=Y;Who=Z;Who=W)).
