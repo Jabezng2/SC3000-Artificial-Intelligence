da5b9e0ede03d919b462dd4d9a2aac5f59fb4379
HLeong12345 <100272517+HLeong12345@users.noreply.github.com>
2023-03-21 16:29:59 +0800

    Delete rivalry.pl

--- Assignment 2/rivalry.pl
+++
@@ -1,9 +0,0 @@
-inIndustry(sumsum, smartPhone).
-inIndustry(appy, smartPhone).
-hold(stevey, galactica-s3).
-develop(sumsum, galactica-s3).
-bossOf(stevey, appy).
-
-rival(A,C):- inIndustry(A,B), inIndustry(C,B), not(string(A)=@=string(C)).
-stole(X,Y):- hold(X,Y), develop(Z,Y), not(bossOf(X,Z)).
-unethical(X):- bossOf(X,Y),rival(Y,Z),develop(Z,A), stole(X,A).
