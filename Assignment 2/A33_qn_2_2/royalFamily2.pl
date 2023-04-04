deb97f38dd79af91c2096345f89ce98b07e3ad7b
dotalim0204 <50509341+dotalim0204@users.noreply.github.com>
2023-04-04 17:09:48 +0800

    Add files via upload

--- Assignment 2/A33_qn_2_2/royalFamily2.pl
+++ Assignment 2/A33_qn_2_2/royalFamily2.pl
@@ -18,7 +18,20 @@ queen(elizabeth).
 % Define facts of order of age(a,b,c,d), where a is older than b, b is older than c, c is older than d.
 age(charles,ann,andrew,edward).
 
+% Define rule for prince(x), where x is a prince if x is a offspring of y, y is a queen and x is a male.
+prince(X) :-
+    offspring(Y,X),
+    queen(Y),
+    male(X).
+
+% Define rule for princess(x), where x is a princess if x is offspring of y, y is a queen and x is a female.
+princess(X) :-
+    offspring(Y,X),
+    queen(Y),
+    female(X).
+
 % Define rule for new successionLine(Who).
 successionLine(Who) :-
     age(X,Y,Z,W),
-    (Who=X;Who=Y;Who=Z;Who=W).
+    (Who=X;Who=Y;Who=Z;Who=W),
+    (prince(Who);princess(Who)).
